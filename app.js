e621e05b095d7b079815b9b1a622b005c107a875
Romualdo <romualdooliver@gmail.com>
2022-10-20 07:58:10 +0200

    desde pc casa

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const hora = document.querySelector('#hora');
 const fecha = document.querySelector('#fecha');
 const boton = document.querySelector('#boton');
+const input = document.querySelector('#input');
+
+console.log(input.value);
+
+let tareas = {};
+let idTarea = 0;
+
 
 boton.addEventListener('click', function(e){
     e.preventDefault();
-    console.log(hora.textContent, fecha.textContent);
+    
+    // console.log(hora.textContent, fecha.textContent);
+    crearTarea(input.value);
 })
 
 function ponerHora(){
@@ -18,6 +27,36 @@ setInterval(() => {
     ponerHora();
 }, 1000);
 
+function crearTarea(e){
+ if (e.trim() === '') {
+    console.log('la tarea esta vacia');
+    input.value = '';
+    return;
+ }
+//  console.log(`Has escrito algo a la hora ${hora.textContent} y fecha ${fecha.textContent}`);
+const tarea = {
+    id : idTarea + 1,
+    texto: input.value,
+    hora: hora.textContent,
+    fecha: fecha.textContent
+}
 
+idTarea = tarea.id;
+
+tareas[tarea.id] = tarea;
+
+console.log(tareas);
+ input.value = '';
+
+ crearDiv();
+}
+
+function crearDiv(){
+    const cajaTareas = document.querySelector('#tareas')
+    const divTarea = document.createElement('div');
+    divTarea.classList.add('tarea')
+    cajaTareas.appendChild(divTarea);
+    
+}
 
 // console.log(moment());
