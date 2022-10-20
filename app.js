5321332956631b3eb8c163a6ce3fc3f69b330ebf
Romualdo <romualdooliver@gmail.com>
2022-10-20 22:53:20 +0200

    desde portatil

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const hora = document.querySelector('#hora');
 const fecha = document.querySelector('#fecha');
 const boton = document.querySelector('#boton');
 const input = document.querySelector('#input');
+const botonCompletar = document.querySelectorAll('#boton_completado')
 
-console.log(input.value);
+
+
+// botonCompletar.addEventListener('click', function(e) {
+//     console.log(e);
+// });
 
 let tareas = {};
 let idTarea = 0;
@@ -15,6 +20,8 @@ boton.addEventListener('click', function(e){
     // console.log(hora.textContent, fecha.textContent);
     crearTarea(input.value);
 })
+
+
 
 function ponerHora(){
     hora.textContent = moment().format('HH:mm:ss');
@@ -48,16 +55,28 @@ tareas[tarea.id] = tarea;
 console.log(tareas);
  input.value = '';
 
- crearDiv(tarea.texto);
+ crearDiv(tarea.texto, tarea.hora, tarea.fecha);
 }
 
-function crearDiv(textTarea){
+function crearDiv(textTarea, horatarea, fechatarea){
     const cajaTareas = document.querySelector('#tareas')
     const divTarea = document.createElement('div');
     const textoTarea = document.createElement('p');
     const divInferior = document.createElement('div')
     const hora_fecha = document.createElement('div');
     const spanHoraTarea = document.createElement('span');
+    const spanFechaTarea = document.createElement('span');
+    const divBotones = document.createElement('div');
+    const divBotonBorrar = document.createElement('div');
+    const divBotonEditar = document.createElement('div');
+    const divBotonCompletado = document.createElement('div');
+    const spanBorrar = document.createElement('span');
+    const spanEditar = document.createElement('span');
+    const spanCompletado = document.createElement('span');
+    const abbrBorrar = document.createElement('abbr');
+    const abbrEditar = document.createElement('abbr');
+    const abbrCompletado = document.createElement('abbr');
+    
 
 
     divInferior.classList.add('inferior');
@@ -69,7 +88,37 @@ function crearDiv(textTarea){
     hora_fecha.classList.add('hora_fecha');
     divInferior.appendChild(hora_fecha);
     spanHoraTarea.setAttribute('id', 'hora_tarea');
+    spanHoraTarea.textContent = horatarea; 
     hora_fecha.appendChild(spanHoraTarea);
+    spanFechaTarea.setAttribute('id', 'fecha_tarea');
+    spanFechaTarea.textContent = fechatarea; //borrar
+    hora_fecha.appendChild(spanFechaTarea);
+    divBotones.classList.add('botones')
+    divInferior.appendChild(divBotones);
+
+    divBotonBorrar.setAttribute('id', 'boton_borrar');
+    divBotones.appendChild(divBotonBorrar);
+    divBotonBorrar.appendChild(abbrBorrar);
+    spanBorrar.classList.add('borrar', 'material-symbols-outlined');
+    spanBorrar.textContent = 'delete'
+    abbrBorrar.appendChild(spanBorrar);
+
+    divBotonEditar.setAttribute('id', 'boton_editar');
+    divBotones.appendChild(divBotonEditar);
+    divBotonEditar.appendChild(abbrEditar);
+    spanEditar.classList.add('editar', 'material-symbols-outlined');
+    spanEditar.textContent = 'edit'
+    abbrEditar.appendChild(spanEditar);
+    
+    divBotonCompletado.setAttribute('id', 'boton_completado');
+    divBotones.appendChild(divBotonCompletado);
+    divBotonCompletado.appendChild(abbrCompletado);
+    spanCompletado.classList.add('Completar', 'material-symbols-outlined');
+    spanCompletado.textContent = 'event_available'
+    abbrCompletado.appendChild(spanCompletado);
+
+    console.log(botonCompletar);
+
     
 }
 
